d9d244adf0555531ef4c3d7df229b087846b5c71
Sean Francis N. Ballais <sfballais123@gmail.com>
2016-02-28 15:17:48 +0800

    Added the webcam show code.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,28 @@
+#include <iostream>
+
 #include <opencv2/objdetect/objdetect.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+
+int main()
+{
+    // Init
+    cv::VideoCapture captureDevice(0);
+    if (!cap.isOpened()) { // Check if we turned the camera on
+        return -1;
+    }
+
+    cv::Mat frame;
+
+    // Display the frames
+    while (true) {
+        cap >> frame;
+        cv::imshow("calc-math-cv", frame);
+
+        if (cv::waitKey(30) >= 0) {
+            break;
+        }
+    }
+
+    return 0;
+}
