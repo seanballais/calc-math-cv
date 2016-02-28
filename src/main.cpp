8933b75be90a3ccc1693e241783aab2961769b8a
Sean Francis N. Ballais <sfballais123@gmail.com>
2016-02-28 16:07:10 +0800

    Added post-process code.

--- src/main.cpp
+++ src/main.cpp
@@ -25,6 +25,7 @@ int main()
             return 0;
         } else if (keyPressed == 'g') { // Grab the frame and process it
             webcamFrame.copyTo(equationFrame);
+            break;
         }
     }
 
@@ -32,6 +33,7 @@ int main()
     cv::cvtColor(equationFrame, equationFrame, cv::COLOR_BGR2GRAY); // Convert to grayscale
     cv::GaussianBlur(equationFrame, equationFrame, cv::Size(11, 11), 0);
     cv::adaptiveThreshold(equationFrame, equationFrame, 255, cv::ADAPTIVE_THRESH_MEAN_C, cv::THRESH_BINARY, 5, 2);
+    cv::bitwise_not(equationFrame, equationFrame);
 
     cv::imshow("calc-math-cv", equationFrame);
     cv::waitKey(0);
